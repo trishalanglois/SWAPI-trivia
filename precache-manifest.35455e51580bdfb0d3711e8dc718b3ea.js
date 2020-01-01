a15a6d2708750f67e48a3fe11cd344dde7436642
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-01-01 16:57:47 -0700

    Updates

---
+++ precache-manifest.35455e51580bdfb0d3711e8dc718b3ea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9d07a32edac0ce868bacf3e9f9bc8e",
+    "url": "/SWAPI-trivia/index.html"
+  },
+  {
+    "revision": "81d300945c2df10be0f3",
+    "url": "/SWAPI-trivia/static/css/main.829e33d6.chunk.css"
+  },
+  {
+    "revision": "5d84a360752aaae5273b",
+    "url": "/SWAPI-trivia/static/js/2.6ea25825.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/SWAPI-trivia/static/js/2.6ea25825.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81d300945c2df10be0f3",
+    "url": "/SWAPI-trivia/static/js/main.7d66eee0.chunk.js"
+  },
+  {
+    "revision": "8b043752c1d2313e1df7",
+    "url": "/SWAPI-trivia/static/js/runtime-main.55dcc5cc.js"
+  },
+  {
+    "revision": "69c1ee317461be09a87bc989e48a7261",
+    "url": "/SWAPI-trivia/static/media/baby-yoda.69c1ee31.jpeg"
+  },
+  {
+    "revision": "695ab299f008793b2dfb5ffedeaa8290",
+    "url": "/SWAPI-trivia/static/media/background-stars.695ab299.jpeg"
+  },
+  {
+    "revision": "1ce9dc9aa10b619cf5441aacd2011a08",
+    "url": "/SWAPI-trivia/static/media/character-picture.1ce9dc9a.jpeg"
+  },
+  {
+    "revision": "4871db8e45b0bf1daa14c032e2079ed1",
+    "url": "/SWAPI-trivia/static/media/darth.4871db8e.jpg"
+  },
+  {
+    "revision": "71dad8b84446f9ca131d1cd84ed3a019",
+    "url": "/SWAPI-trivia/static/media/light-saber-x.71dad8b8.jpeg"
+  },
+  {
+    "revision": "482580480d9e8ca8528d9c71fe629f68",
+    "url": "/SWAPI-trivia/static/media/star-wars-logo.48258048.jpeg"
+  }
+]);
